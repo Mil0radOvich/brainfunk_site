6e744e4d200d0243e1ed564d12027e9188024767
OlegMil0rad <sageretard@gmail.com>
2025-06-14 01:05:07 +0300

    db and settings

--- brainfunk_site/settings.py
+++ brainfunk_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z5-h6hus%b@gh15a9v!c0w)h+i%+8!=1y@_tofcobf5do*h(^g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'Europe/Moscow'
 
@@ -115,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
